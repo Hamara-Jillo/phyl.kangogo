5694f1d2a2e897e6ee6154d7259c6ddf9af4c256
HamaraJillo <jebsam56@gmail.com>
2026-03-14 21:58:51 +0300

    Initial commit 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/phyl.kangogo',
+  base: '/phyl.kangogo/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
